8a1ba0ca0c8c4316df3fa823593513d0b79611f4
Karl Williamson <public@khwilliamson.com>
2013-08-14 21:13:52 -0600

    Make typedef fully typedef
    The regcomp.c struct RExC_state_t has not been usable fully as a
    typedef, requiring the 'struct' at times.  This has caused me, and I
    presume others, wasted time when we forget to use it under those
    circumstances when it should be used, but it's never been a big enough
    issue to cause me to spend tuits on it.  But, working on something else,
    I finally came to the realization of what the problem is.  It is because
    proto.h is #included before regcomp.h is, and so functions that are
    declared in proto.h that have something that is a RExC_state_t as a
    parameter don't know that it is a typedef because that is defined in
    regcomp.h.  A way around this is already used for other similar
    structures, and that is to declare them in perl.h which is always read
    in before proto.h, leaving the definitions to regcomp.h.  Thus proto.h
    knows enough to compile.
    
    The structure was already declared in perl.h; just not typedef'd.
    Otherwise proto.h would not know about it at all.  This patch moves two
    regcomp.c related declarations in perl.h to the same section as the
    others, and changes the one for RExC_state_t to be a typedef.  All the
    'struct' uses are removed.

--- perl.h
+++ perl.h
@@ -2218,9 +2218,6 @@ int isnan(double d);
 
 #endif
 
-struct RExC_state_t;
-struct _reg_trie_data;
-
 typedef MEM_SIZE STRLEN;
 
 #ifdef PERL_MAD
@@ -3289,6 +3286,8 @@ struct regnode_charclass_class;	/* Used in S_* functions in regcomp.c */
 typedef struct regnode_charclass_class regnode_charclass_posixl;
 
 typedef struct regnode_ssc regnode_ssc;
+typedef struct RExC_state_t RExC_state_t;
+struct _reg_trie_data;
 
 struct ptr_tbl_ent {
     struct ptr_tbl_ent*		next;
